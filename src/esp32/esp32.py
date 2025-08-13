296478d89d353d7c9271bb6d163f26cba47e40e4
sweet389 <pedroadf2010@gmail.com>
2025-08-13 18:54:19 -0300

    aa

--- src/esp32/esp32.py
+++ src/esp32/esp32.py
@@ -1,21 +1,2 @@
-import socket
-def socket_setup():
-    global s
-    HOST, PORT = "192.168.0.201", 64000
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((HOST, PORT))
-    s.listen()
-    print("Waiting")
-    global conn, addr
-    conn, addr = s.accept()
-    print(f"Connected with {addr}")
-def socket_recv():
-    data=conn.recv(1024)
-    data=data.decode()
-    if data:
-        print(data)
-def main():
-    socket_setup()
-    for x in range(0,11):
-        socket_recv()
-main()
+import network 
+
